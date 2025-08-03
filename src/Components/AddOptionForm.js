864310c2e273041236cadc4877a3f995a191d452
Büşra K. <ekacar221@hotmail.com>
2025-08-03 12:54:28 +0300

    Düzenlemeler yapıldı.

--- src/Components/AddOptionForm.js
+++ src/Components/AddOptionForm.js
@@ -13,11 +13,11 @@ function AddOptionForm({ addOption }) {
   return (
     <form onSubmit={handleSubmit} style={{ maxWidth: 700, margin: '20px auto', display: 'flex', gap: '10px' }}>
       <input
+        className='seçenek'
         type="text"
         placeholder="Yeni seçenek ekle..."
         value={inputValue}
         onChange={(e) => setInputValue(e.target.value)}
-        style={{ flexGrow: 1, padding: '10px 12px', fontSize: 16, borderRadius: 6, border: '1px solid #ccc' }}
       />
       <Button type="primary" htmlType="submit">Ekle</Button>
     </form>
